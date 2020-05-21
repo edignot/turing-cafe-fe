ff3dc492d89f0c2f7b86614a5768a2c9421ff985
Ed Ignot <edignot@gmail.com>
2020-05-21 11:44:32 -0600

    Reservation card test done

--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -4,7 +4,40 @@ import Card from './Card'
 import '@testing-library/jest-dom'
 
 describe('<Card />', () => {
-    test('Doisplays reservation info correctly', () => {
-        expect(true).toBeTruthy()
+    const cancelReservation = jest.fn()
+    let reservation
+    let cardComponent
+    beforeEach(() => {
+        reservation = {
+            "id": 1,
+            "name": "Christie",
+            "date": "12/29",
+            "time": "7:00",
+            "number": 12
+            }
+
+        cardComponent = render(
+            <Card 
+                reservation={reservation}
+                cancelReservation={cancelReservation}
+            />
+        )
+    })
+
+    test('Displays reservation info correctly', () => {
+        const { getByText } = cardComponent
+        expect(getByText('Christie')).toBeInTheDocument()
+    })
+
+    test('Cancel Reservation has been clicked', () => {
+        const { getByText } = cardComponent
+        fireEvent.click(getByText('CANCEL'))
+        expect(cancelReservation).toHaveBeenCalledTimes(1)
+    })
+
+    test('Cancel Reservation has been invoked with correct arguments', () => {
+        const { getByText } = cardComponent
+        fireEvent.click(getByText('CANCEL'))
+        expect(cancelReservation).toHaveBeenCalledWith(reservation.id)
     })
 })
